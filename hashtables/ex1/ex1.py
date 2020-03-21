c2299417bd09bbb6660b9248e24fb377efa82bb3
Ira <ira.d.sanchez@gmail.com>
2020-03-20 19:41:18 -0700

    Add Finished get_indices_of_item_weights in ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,12 +8,34 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    # start answer as none, so it returns properly if no changes are made
+    answer = None
+    # insert (weight, index) pairs in the hash table
+    for index, weight in enumerate(weights):
+        hash_table_insert(ht, weight, index)
 
-    """
-    YOUR CODE HERE
-    """
+    # now that we've added the items to ht, we can go through and check
+    for index, weight in enumerate(weights):
+        key = limit - weight
 
-    return None
+        # if the key exists
+        if hash_table_retrieve(ht, key) is not None:
+            # reset the answer if this is the first one we've found
+            if answer is None:
+                answer = (index,)
+            else:
+                # add to the answer if it's not the first one
+                answer = answer + (index,)
+
+    # if the answer's been changed at this point at all by the above loop,
+    if answer is not None:
+        # check to see if we have a pair
+        if len(answer) == 2:
+            if answer[1] > answer[0]:
+                # correct the order if not in the right order already
+                answer = (answer[1], answer[0])
+
+    return answer
 
 
 def print_answer(answer):
