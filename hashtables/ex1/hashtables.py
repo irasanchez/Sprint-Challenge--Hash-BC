5d345d89e6ea9d8e68d4b4dc79dc42fce6eb53ec
Ira <ira.d.sanchez@gmail.com>
2020-03-20 23:07:37 -0700

    Add Second Hashtable

--- hashtables/ex1/hashtables.py
+++ hashtables/ex1/hashtables.py
@@ -1,8 +1,8 @@
-
-
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -8,16 +8,36 @@ from hashtables import (HashTable,
 
 class Ticket:
     def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+        self.source = source  # first flight ticket source is "NONE"
+        self.destination = destination  # last flight destination is "NONE"
 
 
 def reconstruct_trip(tickets, length):
+    print("\n\n\n\n\n")
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        # map the tickets to the hash table
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+        if ticket.source == "NONE":
+            # if this is the first one, add it to the route, since we know where it goes. this also provides a starting point for the next part
+            route[0] = ticket.destination
+
+    # put the stops in the route
+    for index, stop in enumerate(route):
+        # set the next item in the list
+        if index != 0:
+            # print("INDEX IS NOT 0 OR LENGTH", index)
+            previous_stop = route[index-1]
+            print("PREV =>", previous_stop)
+            current_stop = hash_table_retrieve(hashtable, previous_stop)
+            route[index] = current_stop
+
+        print(index, route[index])
+
+    # take the last item off, since it'll be set to "NONE"
+    route = route[:-1]
+
+    return route
